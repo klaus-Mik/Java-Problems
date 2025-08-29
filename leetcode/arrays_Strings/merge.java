d224b09d8d79c41ed76e5b8e14cc4097e302bf7e
klaus-Mik <hisravan23@gmail.com>
2025-08-29 10:40:43 +0530

    commit

--- leetcode/arrays_Strings/merge.java
+++ leetcode/arrays_Strings/merge.java
@@ -1,5 +1,40 @@
 package arrays_Strings;
+import java.util.*;
+
+import java.util.ArrayList;
 
 public class merge {
+    public static void mergesorted(int[] a, int[] b){
+        List<Integer> arrays=new ArrayList<>();
+        int i=0;
+        int j=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]){
+                arrays.add(a[i]);
+                i++;
+            }
+            else{
+                arrays.add(b[j]);
+                j++;
+            }
+        }
+        while(i<a.length){
+            arrays.add(a[i]);
+            i++;
+        }
+        while(j<b.length){
+            arrays.add(b[j]);
+            j++;
+        }
+       
+        System.out.println(arrays);
+
+    }
+
+    public static void main(String[] args){
+        int[] a={1,2,6,7,9};
+        int[] b={0,1,2,3,4,5};
+        mergesorted(a, b);
+    }
     
 }
---
+++ leetcode/arrays_Strings/remove.java
@@ -0,0 +1,19 @@
+package arrays_Strings;
+
+public class remove {
+    public static int lengthh(int[] a, int val){
+        int k=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=val){
+                a[k]=a[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args){
+        int[] a={1,1,2,3,4,5,6,7,1};
+        int val=1;
+        System.out.println(lengthh(a, val));
+    }
+}
