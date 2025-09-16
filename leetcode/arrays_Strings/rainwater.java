7a89ce7221ad2ba63373530d41ed412e4174772f
klaus-Mik <hisravan23@gmail.com>
2025-09-17 00:33:58 +0530

    commit

---
+++ leetcode/arrays_Strings/rainwater.java
@@ -0,0 +1,37 @@
+public class rainwater {
+    public static int water(int[] arr){
+        int q=0;
+        int left=0;
+        int n=arr.length;
+        int right=n-1;
+        int i=0;
+        int left_max=0;
+        int right_max=0;
+        while(left<=right){
+            if(arr[left]<=arr[right]){
+                if(right_max>arr[right]){
+                    q+=right_max-arr[right];
+                }
+                else{
+                    right_max=arr[right];
+
+                }
+                right--;
+            } else{
+                if(arr[left]>left_max){
+                    left_max=arr[left];
+                }
+                else{
+                    q+=left_max-arr[left];
+                }
+                left++;
+
+            }  
+        }
+        return q;
+    }
+    public static void main(String[] args){
+        int[] arr={3,1,2,4,0,1,3,2};
+        System.out.println(water(arr));
+    }
+}
---
+++ leetcode/arrays_Strings/roman.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class roman {
+    public static void rom(String x){
+          Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int total=0;
+        for(int i=0;i<x.length();i++;){
+            int value=x.charAt(i);
+            total+=
+
+        }
+
+      
+
+    }
+    public static void main(String[] args){
+
+    }
+}
